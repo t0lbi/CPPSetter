a0c5428f13d4871acca65dd0e5a8c7f7c17b0f48
tolbi <berkeinantol@gmail.com>
2026-08-22 13:04:48 +0300

    added temp/ to .gitignore

--- temp/problem-data/checker.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(int argc, char* argv[]) {
-	// (argv: input, contestant.out, official.out)
-	cout<<0.0<<'
-'; //print double between 0.0 and 1.0
-	return 0;
-}
