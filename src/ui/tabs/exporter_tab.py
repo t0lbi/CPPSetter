d823fa09aba50b6439b2fdb82521470fc5725659
tolbi <berkeinantol@gmail.com>
2026-08-26 20:54:51 +0300

    UI changes

--- src/ui/tabs/exporter_tab.py
+++ src/ui/tabs/exporter_tab.py
@@ -4,10 +4,36 @@ from tkinter import ttk
 class ExporterTab:
     def __init__(self, parent, main):
         self.main = main
-        self.frame = ttk.Frame(parent)
+        self.frame = tk.Frame(parent, bg="#333333")
         
         self._build_ui()
         
     def _build_ui(self):
-        label = tk.Label(self.frame, text="Exporter Tab", font=("Arial", 16))
-        label.pack(pady=50)
+        tk.Label(
+            self.frame,
+            text="Target Platform:",
+            bg="#333333",
+            fg="#ffffff",
+            font=("Arial", 30)
+        ).pack(pady=(150,0))
+        platforms=["Codeforces", "CMS", "Algoleague"]
+        self.platforms_combo = ttk.Combobox(self.frame, values=platforms, state="readonly", font=("Arial", 25))
+        self.platforms_combo.pack(pady=75)
+        self.platforms_combo.bind("<<ComboboxSelected>>", self.yap)
+        
+
+
+        self.export_button = tk.Button(
+            self.frame,
+            text="Export to...",
+            font=("Arial",25),
+            width=15,
+            height=3,
+            bg="#3c3f41",
+            fg="#ffffff",
+            command=self.yap
+        )
+        self.export_button.pack()
+
+    def yap(*args):
+        print("hicbir ise yaramiyorum :(")
--- src/ui/tabs/generators_tab.py
+++ src/ui/tabs/generators_tab.py
@@ -4,10 +4,83 @@ from tkinter import ttk
 class GeneratorsTab:
     def __init__(self, parent, main):
         self.main = main
-        self.frame = ttk.Frame(parent)
-        
+        self.frame = tk.Frame(parent)
         self._build_ui()
         
     def _build_ui(self):
-        label = tk.Label(self.frame, text="Generators Tab", font=("Arial", 16))
-        label.pack(pady=50)
+        self.main_frame = tk.Frame(self.frame, bg="#333333", width=800)
+        self.debug_frame = tk.Frame(self.frame, bg="#333333")
+        self.main_frame.pack_propagate(False)
+        self.main_frame.pack(side="left", fill="both")
+        self.debug_frame.pack(side="right", fill="both", expand=True)
+
+
+        tk.Label(self.main_frame, text="Name:", bg="#333333", fg="#ffffff", font=("TkDefaultFont",12)).pack(anchor="w", padx=5)
+        self.gen_name = tk.Text(self.main_frame, height=1, width=20, bg="#111111", fg="#ffffff", font=("TkDefaultFont",12))
+        self.gen_name.pack(padx=5, pady=5, anchor="w")
+        tk.Button(self.main_frame, text="Create New Generator", bg="#3c3f41", fg="#ffffff", font=("TkDefaultFont",12), command=self.yap).pack(anchor="w", padx=5)
+
+        style = ttk.Style()
+        style.configure(
+            "Treeview",
+            background="#111111",
+            foreground="#ffffff",
+            rowheight=20,
+            fieldbackground="#111111",
+            bordercolor="#333333",
+            borderwidth=1,
+            lightcolor="#333333",
+            darkcolor="#333333"
+        )
+        style.configure(
+            "Treeview.Heading",
+            background="#333333",
+            foreground="#ffffff",
+            bordercolor="#3c3f41",
+            relief="flat",
+            font=("TkDefaultFont", 10, "bold")
+        )
+        columns = (
+            "name",
+        )
+        headings = (
+            "Generator Name",
+        )
+        tree = ttk.Treeview(
+            self.main_frame,
+            columns=columns,
+            show="headings",
+            height=25
+        )
+        for i in range(len(columns)):
+            tree.heading(columns[i], text=headings[i])
+            tree.column(columns[i], width=len(headings[i])*15+5, anchor="center", stretch=False)
+        tree.pack(padx=20, pady=20, fill="both", expand=True)
+
+        gen_frame = tk.Frame(self.main_frame, bg="#333333")
+        gen_frame.pack(fill="x", pady=5)
+        tk.Button(gen_frame, text="Edit", bg="#3c3f41", fg="#ffffff", font=("TkDefaultFont",12), command=self.yap).pack(side="left", padx=5)
+        tk.Button(gen_frame, text="Upload", bg="#3c3f41", fg="#ffffff", font=("TkDefaultFont",12), command=self.yap).pack(side="left", padx=5)
+        tk.Button(gen_frame, text="AI", bg="#3c3f41", fg="#ffffff", font=("TkDefaultFont",12), command=self.yap).pack(side="left", padx=5)
+
+
+        tk.Label(self.main_frame, text="Arguments:", bg="#333333", fg="#ffffff", font=("TkDefaultFont",12)).pack(anchor="w", padx=5)
+        self.gen_args = tk.Text(self.main_frame, height=1, width=20, bg="#111111", fg="#ffffff", font=("TkDefaultFont",12))
+        self.gen_args.pack(padx=5, pady=5, anchor="w")
+        
+        gen_runner_frame = tk.Frame(self.main_frame, bg="#333333")
+        gen_runner_frame.pack(fill="x", pady=5)
+        tk.Button(gen_runner_frame, text="Run", bg="#3c3f41", fg="#ffffff", font=("TkDefaultFont",12), command=self.yap).pack(side="left", padx=5)
+        tk.Button(gen_runner_frame, text="View Input", bg="#3c3f41", fg="#ffffff", font=("TkDefaultFont",12), command=self.yap).pack(side="left", padx=5)
+        
+        
+        input_area = tk.Label(self.debug_frame,font=("Arial", 11), bg="#111111",fg="#ffffff",text="Errors will be shown here.", anchor="nw")
+        input_area.pack(padx=20, pady=20, fill="both", expand=True)
+
+    def update_buttons(self, *args):
+        for button in self.checker_buttons:
+            button["state"] = "normal" if self.checker_var.get() else "disabled"
+        for button in self.grader_buttons:
+            button["state"] = "normal" if self.grader_var.get() else "disabled"
+    def yap(*args):
+        print("hicbir ise yaramiyorum :(")
